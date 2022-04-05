8c448e6d6bcf4b8c7bd659e0547d94812572123c
Martin Stamm <martin.stamm@camunda.com>
2022-04-05 14:36:02 +0200

    fix: add `modelerTemplateIcon` as property

--- test/spec/xml/read.js
+++ test/spec/xml/read.js
@@ -613,15 +613,7 @@ describe('read', function() {
             $type: 'bpmn:Task',
             modelerTemplate: 'foo',
             modelerTemplateVersion: 1,
-            extensionElements: {
-              $type: 'bpmn:ExtensionElements',
-              values: [
-                {
-                  $type: 'zeebe:ModelerTemplateIcon',
-                  body: "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16'%3E%3C/svg%3E",
-                }
-              ]
-            }
+            modelerTemplateIcon: "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16'%3E%3C/svg%3E"
           });
         });
 
